6aaa5048d274d092b8384e6b77f36b33fa888ad4
IsaaFarfan21 <i.farfan.c@uni.pe>
2025-09-11 20:59:35 -0500

    Refactorizado el controlador para utilizar los servicios de producto

--- demo.spring-boot/src/main/java/com/utec/demo/spring_boot/controllers/ProductoController.java
+++ demo.spring-boot/src/main/java/com/utec/demo/spring_boot/controllers/ProductoController.java
@@ -2,40 +2,44 @@ package com.utec.demo.spring_boot.controllers;
 
 import com.utec.demo.spring_boot.producto.Producto;
 import com.utec.demo.spring_boot.producto.ProductoDTO;
+import com.utec.demo.spring_boot.producto.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+import javax.lang.model.element.ModuleElement;
 import java.util.List;
 
 
 @RestController
 @RequestMapping("/api/productos")
 public class ProductoController {
+    private final ProductoService productoService;
+
+    @Autowired
+    public ProductoController(ProductoService productoService) {
+        this.productoService=productoService;
+    }
 
     @GetMapping
     public List<Producto> listaProductos(){
-        return BD;
+        return productoService.listar();
     }
+
     @PostMapping
     public Producto crearProducto(@RequestBody ProductoDTO productoDTO) {
-        Producto nuevoProducto = new Producto(NEXT_ID.getAndIncrement(), productoDTO.getNombre(),
-                productoDTO.getPrecio()) ;
-        BD.add(nuevoProducto);
-        return nuevoProducto;
+        return productoService.crear(productoDTO);
+
     }
     @GetMapping("/{id}")
     public Producto obtenerPorducto(@PathVariable long id)
     {
-        return BD.stream()
-                .filter(p -> p.getId()
-                ==id)
-                .findFirst()
-                .orElse(null);
+        return productoService.obtenerPorId(id);
     }
+
     @DeleteMapping ("/{id}")
     public ResponseEntity<Void> eliminarProducto(@PathVariable long id){
-        boolean notaEliminada= BD.removeIf(p->p.getId()==id);
+        boolean notaEliminada= productoService.eliminar(id);
         if(notaEliminada=true){
             return ResponseEntity.noContent().build();
         }
